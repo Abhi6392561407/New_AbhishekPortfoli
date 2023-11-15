b934366a3c545a2c5f89e2d64a795d2e8bf5e86f
Abhishek <abhi407tiwari@gmail.com>
2023-11-15 13:02:10 +0530

    Your commit message

---
+++ src/data/data.js
@@ -0,0 +1,54 @@
+import Image1 from '../assets/projects/Image1.JPG'
+import Image2 from '../assests/projects/Image1.jpg'
+import Image3 from '../assests/projects/Image3.jpg'
+import WorkIm from '../assets/projects/workImg.jpeg'
+import Image4 from '../assets/projects/Image4.JPG'
+import RealEst from '../assets/projects/realestate.jpg'
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Image1,
+        github:"https://github.com/Abhi6392561407/",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Image2,
+        github:"https://github.com/Abhi6392561407/",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Image3,
+        github:"https://github.com/Abhi6392561407/",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:Image4,
+        github:"https://github.com/Abhi6392561407/",
+        live:"",
+    },
+    {
+        id:5,
+        name:"React JS Application",
+        image:RealEst,
+        github:"https://github.com/Abhi6392561407/",
+        live:"",
+    },
+    {
+        id:6,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"https://github.com/Abhi6392561407/",
+        live:"",
+    },
+
+
+
+
+]
